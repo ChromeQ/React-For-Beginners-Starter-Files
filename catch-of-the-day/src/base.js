e4e82b0a066399ba9dea2d816787eb93f7f9f159
Dav Hill <originaldavz@gmail.com>
2016-12-23 01:13:14 +0000

    Step 18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: 'AIzaSyDtR1FUDnpj_p1WeflzAjOc0dmWjgnjccQ',
+    authDomain: 'catch-of-the-day-dav-hill.firebaseapp.com',
+    databaseURL: 'https://catch-of-the-day-dav-hill.firebaseio.com'
+});
+
+export default base;
